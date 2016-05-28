bd3ccb267ce7cb767217082451d03a54d09d0026
hheennrryy1 <hheennrryy1@icloud.com>
2016-05-28 14:29:53 +0800

    first commit

---
+++ src/main/java/com/henry/entity/Student.java
@@ -0,0 +1,27 @@
+package com.henry.entity;
+
+import java.util.Date;
+
+public class Student {
+	private int id;
+	private String name;
+	private Date birthday;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+}
